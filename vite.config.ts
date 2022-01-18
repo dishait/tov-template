ef81653c928515a96013e1780b4a62cf2886afef
markthree <1801982702@qq.com>
2022-01-18 17:12:38 +0800

    feat(path): 添加路径别名~

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import { resolve } from 'path'
 import Tov from './presets/tov'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			'~/': `${resolve(__dirname, 'src')}/`
+		}
+	},
 	plugins: [Tov()]
 })
