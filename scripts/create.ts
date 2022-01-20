ad41e6c790bf896c01c9a986e6e74486c1f74fe0
markthree <1801982702@qq.com>
2022-01-21 00:24:03 +0800

    refactor: 重构组件自动创建

--- scripts/create.ts
+++ scripts/create.ts
@@ -1,9 +1,62 @@
 import {
+	useInquirerList,
+	useInquirerConfirm,
+	useInquirerQuestion
+} from '@markthree/ilazy'
+import {
 	copy,
-	prompt,
 	createPath,
-	pathExistsSync,
-	createTerminalLoading
+	terminalColor,
+	pathExistsSync
 } from '@markthree/node-shared'
 
-const usePath = createPath(__dirname)
+const p = createPath(__dirname)
+const templates = {
+	component: p('./template/component.vue')
+}
+const pagesPath = p('../src/pages')
+const layoutPath = p('../src/layouts')
+const componentsPath = p('../src/components')
+
+const noticeSuccess = (msg = '创建成功') => {
+	console.log('😋', terminalColor.green(msg))
+}
+
+const noticeFail = (msg = '创建失败') => {
+	console.log('😥', terminalColor.red(msg))
+}
+
+const run = async () => {
+	const type = await useInquirerList(
+		'您要创建以下哪种类型的文件',
+		{
+			default: 'component',
+			choices: ['component', 'page', 'layout']
+		}
+	)
+
+	const isComponent = type === 'component'
+
+	if (isComponent) {
+		const name = await useInquirerQuestion(
+			'输入你要创建的名称'
+		)
+		const dest = createPath(componentsPath)(`${name}.vue`)
+		if (pathExistsSync(dest)) {
+			const shouldCover = await useInquirerConfirm(
+				'文件已存在，是否覆盖?',
+				false
+			)
+
+			if (shouldCover) {
+				await copy(templates.component, dest)
+				return noticeSuccess()
+			}
+			return noticeFail()
+		}
+		await copy(templates.component, dest)
+		noticeSuccess()
+	}
+}
+
+run()
