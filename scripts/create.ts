5fc983098c493f86cb3af0eb54fafc37580fcdb0
markthree <1801982702@qq.com>
2022-01-21 16:07:03 +0800

    feat: 添加页面自动创建

--- scripts/create.ts
+++ scripts/create.ts
@@ -6,57 +6,92 @@ import {
 import {
 	copy,
 	createPath,
+	createFile,
+	readFileSync,
 	terminalColor,
-	pathExistsSync
+	pathExistsSync,
+	templateCompile
 } from '@markthree/node-shared'
 
 const p = createPath(__dirname)
-const templates = {
-	component: p('./template/component.vue')
-}
-const pagesPath = p('../src/pages')
-const layoutPath = p('../src/layouts')
-const componentsPath = p('../src/components')
+
+const useDestDirPath = (type: string) =>
+	p(`../src/${type}s`)
 
 const noticeSuccess = (msg = '创建成功') => {
-	console.log('😋', terminalColor.green(msg))
+	console.log(terminalColor.green(msg))
 }
 
 const noticeFail = (msg = '创建失败') => {
-	console.log('😥', terminalColor.red(msg))
+	console.log(terminalColor.red(msg))
+}
+
+const showGenZh = (t: string) => {
+	const types = {
+		page: '页面',
+		component: '组件'
+	}
+	return types[t] || ''
 }
 
 const run = async () => {
-	const type = await useInquirerList(
-		'您要创建以下哪种类型的文件',
+	const genType = await useInquirerList(
+		'😋 您要创建以下哪种类型的文件?',
 		{
 			default: 'component',
-			choices: ['component', 'page', 'layout']
+			choices: ['page', 'component']
+		}
+	)
+	const type = await useInquirerList(
+		'🧐 请选择您要创建的类型',
+		{
+			default: 'vue',
+			choices: ['md', 'vue']
 		}
 	)
 
-	const isComponent = type === 'component'
+	const name = await useInquirerQuestion(
+		'🤔 请输入您要创建的名字'
+	)
 
-	if (isComponent) {
-		const name = await useInquirerQuestion(
-			'输入你要创建的名称'
-		)
-		const dest = createPath(componentsPath)(`${name}.vue`)
-		if (pathExistsSync(dest)) {
-			const shouldCover = await useInquirerConfirm(
-				'文件已存在，是否覆盖?',
-				false
-			)
+	const bp = createPath(useDestDirPath(genType))
+	const dest = bp(`${name}.${type}`)
+	const mdDest = bp(`${name}.md`)
+	const vueDest = bp(`${name}.vue`)
 
-			if (shouldCover) {
-				await copy(templates.component, dest)
-				return noticeSuccess()
-			}
-			return noticeFail()
-		}
-		await copy(templates.component, dest)
-		noticeSuccess()
+	const genZh = showGenZh(type)
+	const cTip = `😥 存在相同命名的${type}${genZh}文件，是否覆盖?`
+	const shouldGen =
+		isWillCover(mdDest, cTip) || isWillCover(vueDest, cTip)
+	if (shouldGen) {
+		await gen(p(`./template/shared.${type}`), dest, {
+			name,
+			genType
+		})
+		return noticeSuccess()
 	}
+	noticeFail()
+}
+run()
+
+// 是否将覆盖
+const isWillCover = async (
+	dest: string,
+	msg: string = '文件已存在，是否覆盖?'
+) => {
+	if (pathExistsSync(dest)) {
+		return await useInquirerConfirm(msg, false)
+	}
+	return true
 }
 
-run()
+// 生成文件
+const gen = async (
+	src: string,
+	dest: string,
+	payload: any = {}
+) => {
+	let template = readFileSync(src).toString()
+	template = templateCompile(template)(payload)
+	await createFile(dest, template)
+}
---
+++ scripts/template/shared.md
@@ -0,0 +1,3 @@
+# {{name}}
+
+The {{name}} is a {{genType}}!
