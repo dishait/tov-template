659083ead86432c20a110465ffa65aee355a8fd2
markthree <1801982702@qq.com>
2022-02-04 13:51:13 +0800

    chore: 更友好的预设提示

--- presets/tov.ts
+++ presets/tov.ts
@@ -30,41 +30,50 @@ const markdownWrapperClasses =
 
 export default () => {
 	return [
+		// vue 官方插件，用来解析 sfc
 		Vue({
 			include: [/\.vue$/, /\.md$/]
 		}),
+		// markdown 编译插件
 		Markdown({
 			wrapperClasses: markdownWrapperClasses,
 			markdownItSetup(md) {
 				md.use(Prism)
 			}
 		}),
+		// 文件路由
 		Pages({
 			extensions: ['vue', 'md', 'tsx']
 		}),
+		// 布局系统
 		Layouts(),
+		// 调试工具
 		Inspect(),
+		// windicss 插件
 		Windicss({
 			safelist: markdownWrapperClasses
 		}),
+		// mock 服务
 		viteMockServe(),
 		// https://icones.netlify.app/
 		Icons({
 			autoInstall: true
 		}),
+		// 组件自动按需引入
 		Components({
+			extensions: ['vue', 'md', 'tsx'],
+			include: [/\.md$/, /\.vue$/, /\.tsx$/],
 			dts: resolve(__dirname, './types/components.d.ts'),
-			extensions: ['vue', 'md', 'svg', 'tsx'],
-			include: [/\.md$/, /\.vue$/, /\.svg$/, /\.tsx$/],
 			resolvers: [
-				ElementPlusResolver(),
 				IconsResolver({
 					prefix: ''
 				}),
 				NaiveUiResolver(),
+				ElementPlusResolver(),
 				VueUseComponentsResolver()
 			]
 		}),
+		// api 自动按需引入
 		AutoImport({
 			dts: './presets/types/auto-imports.d.ts',
 			imports: [
@@ -80,15 +89,19 @@ export default () => {
 				ElementPlusResolver()
 			]
 		}),
+		// i18n 国际化支持
 		I18n({
 			runtimeOnly: true,
 			compositionOnly: true,
 			include: [resolve(__dirname, '../locales/**')]
 		}),
+		// 预设热重启服务
 		ViteRestart({
 			restart: ['presets/tov.[jt]s', 'presets/shared/**/*']
 		}),
+		// tsx 支持
 		vueJsx(),
+		// 生产环境资源压缩
 		viteCompression()
 	]
 }
