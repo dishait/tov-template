242d19c1a11fb61747315ea65b34fd83ca051eb1
markthree <1801982702@qq.com>
2022-02-05 15:27:11 +0800

    chore: 更新依赖

--- presets/types/components.d.ts
+++ presets/types/components.d.ts
@@ -3,9 +3,7 @@
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
 declare module 'vue' {
-  export interface GlobalComponents {
-    AButton: typeof import('ant-design-vue/es')['Button']
-  }
+	export interface GlobalComponents {}
 }
 
-export { }
+export {}
