22ebe9f3e3473d0d764831cb3215b082efa3b7bb
markthree <1801982702@qq.com>
2022-03-25 19:44:46 +0800

    chore: add bug issue template

--- presets/types/auto-imports.d.ts
+++ presets/types/auto-imports.d.ts
@@ -138,6 +138,7 @@ declare global {
   const useFetch: typeof import('@vueuse/core')['useFetch']
   const useFocus: typeof import('@vueuse/core')['useFocus']
   const useFocusWithin: typeof import('@vueuse/core')['useFocusWithin']
+  const useFoo: typeof import('/code/tov-template/src/composables/useFoo')['default']
   const useFps: typeof import('@vueuse/core')['useFps']
   const useFullscreen: typeof import('@vueuse/core')['useFullscreen']
   const useGeolocation: typeof import('@vueuse/core')['useGeolocation']
