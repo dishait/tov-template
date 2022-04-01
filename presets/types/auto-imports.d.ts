fa2a262be64c61fb99df5582dca375672961a51e
markthree <1801982702@qq.com>
2022-04-01 14:24:49 +0800

    chore: 更新依赖

--- presets/types/auto-imports.d.ts
+++ presets/types/auto-imports.d.ts
@@ -29,6 +29,7 @@ declare global {
   const effectScope: typeof import('vue')['effectScope']
   const EffectScope: typeof import('vue')['EffectScope']
   const extendRef: typeof import('@vueuse/core')['extendRef']
+  const fooStore: typeof import('/code/tov-template/src/stores/fooStore')['default']
   const getActivePinia: typeof import('pinia')['getActivePinia']
   const getCurrentInstance: typeof import('vue')['getCurrentInstance']
   const getCurrentScope: typeof import('vue')['getCurrentScope']
