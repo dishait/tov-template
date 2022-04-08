53f5c7d0f93f7fc3bc7f893433d5c3c2065d4bcf
markthree <1801982702@qq.com>
2022-04-08 20:05:07 +0800

    chore: 更新依赖

--- presets/types/auto-imports.d.ts
+++ presets/types/auto-imports.d.ts
@@ -126,6 +126,7 @@ declare global {
   const useCssVars: typeof import('vue')['useCssVars']
   const useCycleList: typeof import('@vueuse/core')['useCycleList']
   const useDark: typeof import('@vueuse/core')['useDark']
+  const useDarks: typeof import('/Code/Work/tov-template/src/composables/useDarks')['default']
   const useDateFormat: typeof import('@vueuse/core')['useDateFormat']
   const useDebounce: typeof import('@vueuse/core')['useDebounce']
   const useDebouncedRefHistory: typeof import('@vueuse/core')['useDebouncedRefHistory']
