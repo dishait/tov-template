bb43a5b6726aad04b19bf62890e02d69d8bb1f0d
markthree <1801982702@qq.com>
2022-07-18 17:13:24 +0800

    chore: 更好的 vite3 兼容

--- presets/types/components.d.ts
+++ presets/types/components.d.ts
@@ -3,11 +3,11 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
+export {}
+
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
 }
-
-export {}
--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -2,12 +2,15 @@ import type { App } from 'vue'
 import { createI18n } from 'vue-i18n'
 
 const messages = Object.fromEntries(
-	Object.entries(import.meta.globEager('../../locales/*.y(a)?ml')).map(
-		([key, value]) => {
-			const yaml = key.endsWith('.yaml')
-			return [key.slice(14, yaml ? -5 : -4), value.default]
-		}
-	)
+	Object.entries(
+		import.meta.glob('../../locales/*.y(a)?ml', {
+			eager: true,
+		})
+	).map(([key, value]) => {
+		const yaml = key.endsWith('.yaml')
+		// @ts-ignore
+		return [key.slice(14, yaml ? -5 : -4), value.default]
+	})
 )
 
 export default (app: App) => {
