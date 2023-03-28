c03c101e3179129539ca2033961a74ab7d6bd3d2
markthree <1801982702@qq.com>
2023-03-28 12:41:10 +0800

    fix: 修复嵌套路由下的布局系统失效

--- presets/types/type-router.d.ts
+++ presets/types/type-router.d.ts
@@ -38,7 +38,6 @@ declare module 'vue-router/auto/routes' {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/[...notFound]': RouteRecordInfo<'/[...notFound]', '/:notFound(.*)', { notFound: ParamValue<true> }, { notFound: ParamValue<false> }>,
     '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
-    '/foo': RouteRecordInfo<'/foo', '/foo', Record<never, never>, Record<never, never>>,
   }
 }
 
