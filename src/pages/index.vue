28817e20020f85fbcd000dd63237d09a2bb91cd8
markthree <1801982702@qq.com>
2023-04-29 18:29:14 +0800

    perf: 压缩所有示例图片

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,7 @@
 			<img src="/vite.svg" class="logo" alt="Vite logo" />
 		</a>
 		<a href="https://vuejs.org/" target="_blank">
-			<img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
 		</a>
 	</div>
 	<HelloWorld msg="Tov + Vite + Vue" />
