3c1e7d7f1e733a067e6cf06396d049ba3ff44dc9
markthree <1801982702@qq.com>
2023-05-16 14:05:58 +0800

    refactor: 更声明式的进度条

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 defineOptions({
-	name: 'counter',
+	name: 'Counter',
 })
 const counter = createCounter()
 </script>
--- src/modules/nprogress.ts
+++ src/modules/nprogress.ts
@@ -2,13 +2,8 @@ import { router } from './router'
 import { useNProgress } from '@vueuse/integrations/useNProgress'
 
 // https://vueuse.org/integrations/useNProgress/
-export default () => {
-	const { isLoading } = useNProgress()
+const { start, done } = useNProgress()
 
-	router.beforeEach(() => {
-		isLoading.value = true
-	})
-	router.afterEach(() => {
-		isLoading.value = false
-	})
-}
+router.beforeEach(() => start())
+
+router.afterEach(() => done(true))
