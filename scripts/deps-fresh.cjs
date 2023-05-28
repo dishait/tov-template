255488a2cad7add0e421773681cacb59c09b9c8d
markthree <1801982702@qq.com>
2023-05-28 14:10:25 +0800

    chore!: 更精简的自动依赖更新

---
+++ scripts/deps-fresh.cjs
@@ -0,0 +1,36 @@
+const { execSync } = require('child_process')
+
+/**
+ * 自动更新依赖
+ * @param {import('plop').NodePlopAPI} plop
+ */
+function depsFresh(plop) {
+	plop.setGenerator('controller', {
+		description: '自动更新依赖',
+		prompts: [
+			{
+				name: 'type',
+				type: 'list',
+				default: 'patch',
+				message: '你希望发布一个什么版本?',
+				choices: ['patch', 'minor', 'major'],
+			},
+			{
+				name: 'shouldWrite',
+				type: 'confirm',
+				default: false,
+				message: '是否直接更新?',
+			},
+		],
+		actions(answer) {
+			const { type, shouldWrite } = answer
+
+			execSync(`npx taze ${type} ${shouldWrite ? '-w' : ''}`, {
+				stdio: 'inherit',
+			})
+			return []
+		},
+	})
+}
+
+module.exports = depsFresh
