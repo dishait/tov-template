0390f993930beb4e81baf53500afa61d955c52a2
markthree <1801982702@qq.com>
2023-06-17 17:02:54 +0800

    feat(unocss): 前缀组支持

--- uno.config.ts
+++ uno.config.ts
@@ -8,9 +8,10 @@ import {
 
 import presetAutoprefixer from 'unocss-preset-autoprefixer'
 import transformerDirectives from '@unocss/transformer-directives'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 export default defineConfig({
-	transformers: [transformerDirectives()],
+	transformers: [transformerDirectives(), transformerVariantGroup()],
 	presets: [
 		presetAttributify(),
 		presetIcons({
