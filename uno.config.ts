5cf56e9891dea46745959c3680796cdcb577b02f
API MAN <w2606662632@gmail.com>
2023-11-24 17:52:22 +0800

    chore: remove '@unocss/transformer-directives' and '@unocss/transformer-variant-group' (#125)
    * chore(dependencies): remove '@unocss/transformer-directives' and '@unocss/transformer-variant-group', merge 'transformerVariantGroup,transformerDirectives' into main package
    
    - Deleted '@unocss/transformer-directives' and '@unocss/transformer-variant-group' dependencies.
    - Merged functionality from 'transformerVariantGroup,transformerDirectives' dependency into the main package.
    
    Update unocss configuration to exclude 'transformerVariantGroup,transformerDirectives' dependency content.
    
    * chore(deps): Update pnpm-lock.yaml

--- uno.config.ts
+++ uno.config.ts
@@ -4,11 +4,11 @@ import {
 	presetIcons,
 	presetTypography,
 	presetUno,
+	transformerVariantGroup,
+	transformerDirectives,
 } from 'unocss'
 
 import presetAutoprefixer from './presets/autoprefixer'
-import transformerDirectives from '@unocss/transformer-directives'
-import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 export default defineConfig({
 	transformers: [transformerDirectives(), transformerVariantGroup()],
