6cc841893835823cf0a6de3f3d8f71fc0960d585
luoqiz <luoqiangzheng@gmail.com>
2023-12-22 19:22:24 +0800

    feat: 多文件加载多语言配置 (#135)
    Co-authored-by: luoqiz <luoqiangzheng@gamil.com>

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,16 +1,22 @@
 import { createI18n } from 'vue-i18n'
+import _merge from 'lodash/merge'
 
-const messages = Object.fromEntries(
-	Object.entries(
-		import.meta.glob('../../locales/*.y(a)?ml', {
-			eager: true,
-		}),
-	).map(([key, value]) => {
-		const yaml = key.endsWith('.yaml')
-		// @ts-ignore
-		return [key.slice(14, yaml ? -5 : -4), value.default]
+const languages = Object.entries(
+	import.meta.glob('../../locales/*/**.y(a)?ml', {
+		eager: true,
 	}),
-)
+).map(([key, value]) => {
+	const yaml = key.endsWith('.yaml')
+	// @ts-ignore
+	return [key.slice(14, yaml ? -5 : -4), value.default]
+})
+
+const messages = {}
+
+for (const [key, value] of languages) {
+	const mergerKey = key.split('/')[0]
+	_merge(messages, { [`${mergerKey}`]: value })
+}
 
 // localStorage 中的 locale，第二个参数为默认值
 // https://vueuse.org/core/useStorage/#usestorage
