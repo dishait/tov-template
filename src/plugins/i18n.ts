71e792cb370b0fec5ff7d7cf3f2581c2547c3549
markthree <1801982702@qq.com>
2023-12-23 14:46:27 +0800

    perf: 简化 i18n 模块逻辑，使用 defu 替换 lodash/merge

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,22 +1,22 @@
+import { defu } from 'defu'
 import { createI18n } from 'vue-i18n'
-import _merge from 'lodash/merge'
 
-const languages = Object.entries(
-	import.meta.glob('../../locales/*/**.y(a)?ml', {
-		eager: true,
-	}),
-).map(([key, value]) => {
-	const yaml = key.endsWith('.yaml')
-	// @ts-ignore
-	return [key.slice(14, yaml ? -5 : -4), value.default]
+const yamls = import.meta.glob('../../locales/*/**.y(a)?ml', {
+	eager: true,
 })
 
-const messages = {}
+const languages = Object.entries(yamls).map(([key, value]) => {
+	const yaml = key.endsWith('.yaml')
+	key = key.slice(14, yaml ? -5 : -4)
+	// 如果有子模块，则分割
+	if (key.includes('/')) {
+		key = key.split('/')[0]
+	}
+	// @ts-ignore
+	return { [key]: value.default }
+})
 
-for (const [key, value] of languages) {
-	const mergerKey = key.split('/')[0]
-	_merge(messages, { [`${mergerKey}`]: value })
-}
+const messages = defu({}, ...languages)
 
 // localStorage 中的 locale，第二个参数为默认值
 // https://vueuse.org/core/useStorage/#usestorage
