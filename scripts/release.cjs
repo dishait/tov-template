32c6e43ef4e2689f53e2523a9037125c0f72f3e0
markthree <1801982702@qq.com>
2024-02-19 12:58:59 +0800

    fix(release): 发布前检测 repository 是否正确

--- scripts/release.cjs
+++ scripts/release.cjs
@@ -1,40 +1,51 @@
+const { createConsola } = require('consola')
 const { execSync } = require('child_process')
+const { repository } = require('../package.json')
+const { gray } = require('kolorist')
+
+const logger = createConsola().withTag('release')
 
 /**
  * 自动发版
  * @param {import('plop').NodePlopAPI} plop
  */
-function release(plop) {
-	plop.setGenerator('controller', {
-		description: '自动发版',
-		prompts: [
-			{
-				name: 'type',
-				type: 'list',
-				default: 'patch',
-				message: '你希望发布一个什么版本?',
-				choices: [
-					'patch',
-					'minor',
-					'major',
-					'prepatch',
-					'premajor',
-					'preminor',
-					'prerelease',
-				],
-			},
-		],
-		actions(answer) {
-			const { type } = answer
-			execSync(
-				`npx changelogen --${type} --release && git push --follow-tags`,
-				{
-					stdio: 'inherit',
-				},
-			)
-			return []
-		},
+async function release(plop) {
+	// TODO 从 git 中获取仓库 url
+	const release = await logger.prompt(`是否发布到 ${gray(repository.url)}`, {
+		type: 'confirm',
 	})
+	if (release) {
+		plop.setGenerator('controller', {
+			description: '自动发版',
+			prompts: [
+				{
+					name: 'type',
+					type: 'list',
+					default: 'patch',
+					message: '你希望发布一个什么版本?',
+					choices: [
+						'patch',
+						'minor',
+						'major',
+						'prepatch',
+						'premajor',
+						'preminor',
+						'prerelease',
+					],
+				},
+			],
+			actions(answer) {
+				const { type } = answer
+				execSync(
+					`npx changelogen --${type} --release && git push --follow-tags`,
+					{
+						stdio: 'inherit',
+					},
+				)
+				return []
+			},
+		})
+	}
 }
 
 module.exports = release
