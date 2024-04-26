3760a2d4f722ab04fa05899cdfba8099d658207e
markthree <1801982702@qq.com>
2024-04-26 14:58:44 +0800

    fix: 修复 git 上传代码报错, close #175

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -7,7 +7,7 @@ const counter = createCounter()
 
 <template>
 	<button
-		class="inline-block cursor-pointer rounded from-pink-500 via-red-500 to-yellow-500 bg-gradient-to-r px-8 py-2 text-sm font-medium text-white transition !border-0 !outline-none"
+		class="inline-block cursor-pointer rounded from-pink-500 via-red-500 to-yellow-500 bg-gradient-to-r px-8 py-2 text-sm text-white font-medium transition !border-0 !outline-none"
 		hover="scale-110 shadow-xl"
 		@click="counter.inc()"
 	>
